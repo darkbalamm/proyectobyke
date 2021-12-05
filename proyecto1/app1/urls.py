19c1eb17f658c684781a4fa1ae93e1502062d664
darkbalamm <felsoogod@gmail.com>
2021-12-04 22:46:41 -0600

    crear paginas de cada articulo y relacionar urls en django :C

--- proyecto1/app1/urls.py
+++ proyecto1/app1/urls.py
@@ -3,8 +3,50 @@ from django.urls import path
 from app1 import views
 
 urlpatterns = [
-    path('',views.inicio, name="inicio"),
-    path('piezas',views.piezas, name="piezas"),
+    path('',views.inicio, name="inicio"),  
     path('comentarios',views.comentarios, name="comentarios"),
     path('estadisticas',views.estadisticas, name="estadisticas"),
+
+    path('manubrio',views.manubrio,name="manubrio"),
+    path('horquilla',views.horquilla, name="horquilla"),
+    path('man',views.man, name="man"),
+    path('puños',views.puños, name="puños"),
+    path('poder',views.poder, name="poder"),
+
+    path('cuadros',views.cuadros,name="cuadros"),
+    path('tsup',views.tsup,name="tsup"),
+    path('tinf',views.tinf,name="tinf"),
+    path('tsillin',views.tsillin,name="tsillin"),
+    path('plato',views.plato,name="plato"),
+    path('vinf',views.vinf,name="vinf"),
+    path('vsup',views.vsup,name="vsup"),
+    path('biela',views.biela,name="biela"),
+    path('pedal',views.pedal,name="pedal"),
+    path('telescopio',views.telescopio,name="telescopio"),
+
+    path('ruedas',views.ruedas,name="ruedas"),
+    path('rayos',views.rayos,name="rayos"),
+    path('buje',views.buje,name="buje"),
+    path('llanta',views.llanta,name="llanta"),
+    path('cubierta',views.cubierta,name="cubierta"),
+    path('valvula',views.valvula,name="valvula"),
+    path('piñones',views.piñones,name="piñones"),
+
+    
+    path('asiento',views.asiento,name="asiento"),
+    path('sillin',views.sillin,name="sillin"),
+    path('tija',views.tija,name="tija"),
+
+    path('frenos',views.frenos,name="frenos"),
+    path('fdel',views.fdel,name="fdel"),
+    path('ftrac',views.ftrac,name="ftrac"),
+
+    path('cambios',views.cambios,name="cambios"),
+    path('cplato',views.cplato,name="cplato"),
+    path('cpiñon',views.cpiñon,name="cpiñon"),
+
+    path('accesorios',views.accesorios,name="accesorios"),
+    path('cadena',views.cadena,name="cadena"),
+
+    path('mas',views.mas,name="mas"),
 ]
--- proyecto1/app1/views.py
+++ proyecto1/app1/views.py
@@ -5,10 +5,6 @@ def inicio(request):
 
     return render(request,"app1/inicio.html")
 
-def piezas(request):
-    
-    return render(request,"app1/piezas.html")
-
 def comentarios(request):
     
     return render(request,"app1/comentarios.html")
@@ -16,3 +12,105 @@ def comentarios(request):
 def estadisticas(request):
     
     return render(request,"app1/estadisticas.html")
+def manubrio(request):
+
+    return render(request,"app1/manubrio.html")
+def horquilla(request):
+    
+    return render(request,"app1/horquilla.html")
+def man(request):
+    
+    return render(request,"app1/man.html")
+def puños(request):
+    
+    return render(request,"app1/puños.html")
+def poder(request):
+    
+    return render(request,"app1/poder.html")
+def cuadros(request):
+
+    return render(request,"app1/cuadros.html")
+def tsup(request):
+
+    return render(request,"app1/t-sup.html")
+def tinf(request):
+
+    return render(request,"app1/t-inf.html")
+def tsillin(request):
+
+    return render(request,"app1/t-sillin.html")
+def plato(request):
+
+    return render(request,"app1/plato.html")
+def vsup(request):
+
+    return render(request,"app1/v-sup.html")
+def vinf(request):
+
+    return render(request,"app1/v-inf.html")
+def biela(request):
+
+    return render(request,"app1/biela.html")
+def pedal(request):
+
+    return render(request,"app1/pedal.html")
+def telescopio(request):
+
+    return render(request,"app1/telescopio.html")
+def ruedas(request):
+
+    return render(request,"app1/ruedas.html")
+def rayos(request):
+
+    return render(request,"app1/rayos.html")
+def buje(request):
+
+    return render(request,"app1/buje.html")
+def llanta(request):
+
+    return render(request,"app1/llanta.html")
+def cubierta(request):
+
+    return render(request,"app1/cubierta.html")
+def valvula(request):
+
+    return render(request,"app1/valvula.html")
+def piñones(request):
+
+    return render(request,"app1/piñones.html")
+def asiento(request):
+
+    return render(request,"app1/asiento.html")
+def sillin(request):
+
+    return render(request,"app1/sillin.html")
+def tija(request):
+
+    return render(request,"app1/tija.html")
+def frenos(request):
+
+    return render(request,"app1/frenos.html")
+def fdel(request):
+
+    return render(request,"app1/fdel.html")
+def ftrac(request):
+
+    return render(request,"app1/ftrac.html")
+def cambios(request):
+
+    return render(request,"app1/cambios.html")
+def cplato(request):
+
+    return render(request,"app1/cplato.html")
+def cpiñon(request):
+
+    return render(request,"app1/cpiñon.html")
+def accesorios(request):
+
+    return render(request,"app1/accesorios.html")
+def cadena(request):
+
+    return render(request,"app1/cadena.html")
+def mas(request):
+
+    return render(request,"app1/mas.html")
