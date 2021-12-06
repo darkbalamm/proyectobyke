376a06bd69b7f0ef677f845357b93622b76d640a
darkbalamm <felsoogod@gmail.com>
2021-12-05 22:36:53 -0600

    detalles 2

--- proyecto1/proyecto1/settings.py
+++ proyecto1/proyecto1/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS=[BASE_DIR,"/proyecto1/app1/static" ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
